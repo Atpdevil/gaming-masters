e1bee026eaba9748316479652bc495c9f9de3712
Atpdevil <gokulteck@gmail.com>
2025-11-11 22:31:26 +0530

    Updated

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gamingmasters.app',
+  appName: 'Gaming Masters',
+  webDir: 'build'
+};
+
+export default config;
